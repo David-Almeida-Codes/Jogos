87f636ff5743b84dec1b4cef7a6f5fa61a94bfa0
David Almeida <davidsouzacontato@gmail.com>
2019-08-20 13:08:48 -0300

    preparing hangman_game

--- hangman_game.py
+++ hangman_game.py
@@ -5,6 +5,18 @@ def play():
     print("  WELCOME TO THE HANGMAN GAME! ")
     print("********************************")
 
+    secret_word = "banana"
+
+    hanged = False
+    hit = False
+
+    while(not hanged and not hit):
+        print("Jogando..")
+
+
+
+
+
     print("End of the game!")
 
 if(__name__ == "__main__"):
